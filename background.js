a45c94e0cb755cd6266e81586aa498a25fef6125
joshwaiam <joshwaiam@gmail.com>
2022-05-15 14:06:03 -0400

    Removed script injected logic

--- background.js
+++ background.js
@@ -1,8 +1,3 @@
-let scriptInjected = false
-
 browser.browserAction.onClicked.addListener(() => {
-  if (!scriptInjected) {
-    browser.tabs.executeScript({ file: 'content-script.js' })
-    scriptInjected = true
-  }
+  browser.tabs.executeScript({ file: 'content-script.js' })
 })
